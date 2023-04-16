f5c845c4025d7e8ce929133a9807218e1e3613c6
Oganes <ov_ovanisan@outlook.com>
2023-04-16 17:07:16 +0300

    v1.2

--- app/src/main/java/com/example/music3/MainActivity2.java
+++ app/src/main/java/com/example/music3/MainActivity2.java
@@ -37,7 +37,7 @@ public class MainActivity2 extends BaseActivity {
     }
 
     // отработка переключателя
-    public void switchMethod() {
+    public void switchMethod()  {
         switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
             SharedPreferences save = getSharedPreferences("Save", MODE_PRIVATE);
             SharedPreferences.Editor editor = save.edit();
@@ -46,11 +46,29 @@ public class MainActivity2 extends BaseActivity {
                 editor.putBoolean("mus", true);
                 editor.putBoolean("musStop", false);
                 editor.putBoolean("switch", true);
-                int position = save.getInt("position", 0);
                 editor.apply();
+
+                int position = save.getInt("position", 0);
+                // постепенное увеличение громкости в течение  мсек
+                //  Учитывай что идет задержка переключение свича(кнопки) !!!!!!!
+                int duration = 100;
+                float volume = 0.1f;
+                float maxVolume = 1.0f;
+                float volumeStep = (maxVolume - volume) / duration;
+                float currentVolume = volume;
+                musicSound.setVolume(currentVolume, currentVolume);
                 musicSound.start();
                 musicSound.seekTo(position);
                 musicSound.setLooping(true);
+                for (int i = 0; i < duration; i++) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    currentVolume += volumeStep;
+                    musicSound.setVolume(currentVolume, currentVolume);
+                }
             } else {
                 editor.putBoolean("mus", false);
                 editor.putBoolean("musStop", true);
@@ -64,6 +82,7 @@ public class MainActivity2 extends BaseActivity {
             }
         });
     }
+
     public void go3(View view) {
         Intent intent = new Intent(this, MainActivity3.class);
         startActivity(intent);
